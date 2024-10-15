d13555f7e68cacf75258a7f728538d8f280267f1
k-onoue <k.onoue0724@gmail.com>
2024-10-16 00:57:42 +0900

    Update

--- utils_dev/generate_yml.py
+++ utils_dev/generate_yml.py
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     requirements_file = "requirements.txt"
     requirements = parse_requirements(requirements_file)
     python_version = get_python_version()
-    environment_yml_content = generate_environment_yml(requirements, python_version=python_version, env_name="hovr-bo")
+    environment_yml_content = generate_environment_yml(requirements, python_version=python_version, env_name="hovr-env")
     save_to_yml(environment_yml_content)
