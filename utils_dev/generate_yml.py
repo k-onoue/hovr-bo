a9c4a42042b9838f7d536f043b6dde778c9c6244
k-onoue <k.onoue0724@gmail.com>
2024-10-23 00:50:43 +0900

    fix

--- utils_dev/generate_yml.py
+++ utils_dev/generate_yml.py
@@ -6,13 +6,14 @@ def parse_requirements(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
     dependencies = []
-    for line in lines:
+    for i, line in enumerate(lines):
         line = line.strip()
         if line and not line.startswith('#'):
             # SSH URLをHTTPSに置き換え
-            line = re.sub(r'ssh://git@github\.com/', r'https://github.com/', line)
-            dependencies.append(line)
-    return dependencies
+            new_line = re.sub(r'ssh://git@github\.com/', r'https://github.com/', line)
+            dependencies.append(new_line)
+            lines[i] = new_line + '\n'  # requirements.txtを更新するために行を保存
+    return dependencies, lines
 
 def get_python_version():
     version = f"{sys.version_info.major}.{sys.version_info.minor}"
@@ -36,6 +37,11 @@ def save_to_yml(content, file_path="environment.yml"):
         file.write(content)
     print(f"environment.yml has been created at {file_path}")
 
+def save_to_requirements(lines, file_path="requirements.txt"):
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
+    print(f"requirements.txt has been updated at {file_path}")
+
 if __name__ == "__main__":
     # argparseの設定
     parser = argparse.ArgumentParser(description="Generate environment.yml from requirements.txt.")
@@ -46,11 +52,14 @@ if __name__ == "__main__":
     requirements_file = "requirements.txt"
     
     # 必要な情報を取得
-    requirements = parse_requirements(requirements_file)
+    requirements, updated_lines = parse_requirements(requirements_file)
     python_version = get_python_version()
     
     # environment.ymlの生成
     environment_yml_content = generate_environment_yml(requirements, python_version=python_version, env_name=args.env_name)
     
-    # ファイルに保存
+    # environment.ymlをファイルに保存
     save_to_yml(environment_yml_content)
+
+    # 更新されたrequirements.txtを上書き保存
+    save_to_requirements(updated_lines, requirements_file)
