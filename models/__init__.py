b0d755996d73764c9c938be2d5b3931d4eac93e2
k-onoue <k.onoue0724@gmail.com>
2024-12-12 23:53:32 +0900

    delete

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .dkl import MultiTaskDKL, SingleTaskDKL
-from .ensemble import Ensemble
-from .gp import MultiTaskGP, SingleTaskGP
-from .hmc import HMC
-from .ibnn import MultiTaskIBNN, SingleTaskIBNN
-from .laplace import LaplaceBNN
-from .sghmc import SGHMCModel
